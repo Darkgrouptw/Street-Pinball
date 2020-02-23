8aa0b68d2e4c6ebcae0390effd53d9764d895ddd
Dark <darkgrouptw@gmail.com>
2020-02-23 11:07:35 +0800

    更新場警與 UI 完成的結果

---
+++ Assets/Scripts/ShootEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootEvent : MonoBehaviour
+{
+	[Header("===== Pinball =====")]
+	public GameObject Ball;
+	public GameObject Pin;
+
+	[Header("===== Params =====")]
+	public InputField PowerField;
+	public InputField Angle0Field;
+	public InputField Angle1Field;
+	public InputField AutoTimesField;
+
+	public void Shoot()
+	{
+	}
+}
